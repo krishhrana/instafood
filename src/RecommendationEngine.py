55e92fdbe613304b2661fbcd825d1251a2861a24
Krish Rana <51706825+krishhrana@users.noreply.github.com>
2022-12-20 22:13:42 -0500

    Update RecommendationEngine.py

--- src/RecommendationEngine.py
+++ src/RecommendationEngine.py
@@ -51,12 +51,12 @@ def main():
 # onion garlic hummus egg bread tortilla
 
 if __name__ == '__main__':
-    if not os.path.exists("./app/instafood/src/DataSets"):
-        with zipfile.ZipFile("./app/instafood/src/DataSets.zip", 'r') as zip_ref:
+    if not os.path.exists("src/DataSets"):
+        with zipfile.ZipFile("src/DataSets.zip", 'r') as zip_ref:
             zip_ref.extractall()
             time.sleep(10)
     if not os.path.exists("models"):
-        with zipfile.ZipFile("./app/instafood/src.models.zip", 'r') as zip_ref:
+        with zipfile.ZipFile("src/models.zip", 'r') as zip_ref:
             zip_ref.extractall()
             time.sleep(10)
     model = RecommendationModel(data_path='./DataSets/Clean and Clustered Data/food_data_clustered.csv',
